aa090825ca96987b96ae43c1b1e90618b5a86b3a
nzuwera <nzuwera2002@gmail.com>
2019-10-27 14:21:00 +0200

    initial commit

---
+++ src/main/java/com/goltd/agrigoussd/AgrigoussdApplication.java
@@ -0,0 +1,13 @@
+package com.goltd.agrigoussd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrigoussdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgrigoussdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goltd/agrigoussd/AgrigoussdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goltd.agrigoussd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrigoussdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
