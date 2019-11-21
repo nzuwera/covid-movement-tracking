3fa1f9f26b7b9f35b493bd1a490a5f5f382592ca
nzuwera <nzuwera2002@gmail.com>
2019-11-21 13:56:26 +0200

    Added formatListMenus

--- src/main/java/com/goltd/agrigoussd/helpers/formatter/ListFormatter.java
+++ src/main/java/com/goltd/agrigoussd/helpers/formatter/ListFormatter.java
@@ -6,7 +6,7 @@ import com.goltd.agrigoussd.helpers.UTKit;
 
 import java.util.List;
 
-public class ListFormatter{
+public class ListFormatter {
 
     private ListFormatter() {
         //
@@ -15,8 +15,10 @@ public class ListFormatter{
 
     public static String formatListMenus(String header, List<UssdMenu> listObject) {
         StringBuilder listMessage = new StringBuilder();
-        listMessage.append(header);
-        listMessage.append(UTKit.EOL);
+        if (!header.trim().equals("") && header.trim().length() > 0) {
+            listMessage.append(header);
+            listMessage.append(UTKit.EOL);
+        }
         for (int i = 0; i < listObject.size(); i++) {
             listMessage.append(i + 1);
             listMessage.append(". ");
@@ -26,10 +28,27 @@ public class ListFormatter{
         return listMessage.toString();
     }
 
+    public static StringBuilder formatListMenus(List<UssdMenu> listObject) {
+        StringBuilder listMessage = new StringBuilder();
+        if (listObject.size() > 1) {
+            for (int i = 0; i < listObject.size(); i++) {
+                listMessage.append(i + 1);
+                listMessage.append(UTKit.DOT + UTKit.BLANK);
+                listMessage.append(listObject.get(i).getTitleKin());
+                listMessage.append(UTKit.EOL);
+            }
+        } else {
+            listMessage.append(listObject.get(0).getTitleKin());
+        }
+        return listMessage;
+    }
+
     public static StringBuilder formatLocations(String header, List<Location> listObject) {
         StringBuilder listMessage = new StringBuilder();
-        listMessage.append(header);
-        listMessage.append(UTKit.EOL);
+        if (!header.trim().equals("") && header.trim().length() > 0) {
+            listMessage.append(header);
+            listMessage.append(UTKit.EOL);
+        }
         for (int i = 0; i < listObject.size(); i++) {
             listMessage.append(i + 1);
             listMessage.append(". ");
